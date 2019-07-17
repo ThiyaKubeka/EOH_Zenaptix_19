72d93a79e3de9af1a6bf40e124520279d2c63dc0
Thiyasizwe <thiyasizwe24@gmail.com>
2019-07-17 11:29:54 +0200

    Demo Features

---
+++ Demo features/V1.py
@@ -0,0 +1,83 @@
+import asyncio
+import time
+import re
+import json
+import pprint
+
+
+from indy import crypto, did, wallet,pool,ledger
+from indy.error import IndyError, ErrorCode
+from utils import get_pool_genesis_txn_path, PROTOCOL_VERSION
+from Write_DID_Func import pool_configuration,create_steward_wallet, Steward_did_and_verkey,create_faber_wallet,Faber_did_and_verkey,steward_keys_for_faber,add_to_ledger,print_log
+
+
+
+async def prep(Wallet_handle, steward_verkey, steward_verkey_for_faber, msg):
+    msg = bytes(msg, "utf-8")
+    encrypted = await crypto.auth_crypt(Wallet_handle, steward_verkey, steward_verkey_for_faber, msg)
+    #encrypted = await crypto.anon_crypt(steward_verkey, msg)
+    print('encrypted = %s' % repr(encrypted))
+    with open('message.dat', 'wb') as f:
+        f.write(encrypted)
+    print('prepping %s' % msg)
+
+
+async def init():
+    
+    me = input('Who are you? ').strip()
+    if me == 'steward':
+        pool_handle = await pool_configuration()
+        Wallet_handle = await create_steward_wallet()
+        steward_did,steward_verkey = await Steward_did_and_verkey(Wallet_handle)    
+        
+        faber = input("Faber's DID and verkey? ").strip().split(' ')
+        return Wallet_handle, steward_did, steward_verkey,faber[0],faber[1]
+    elif me == 'faber':
+        pool_handle = await pool_configuration()
+        time.sleep(1)
+        Wallet_handle = await create_steward_wallet()
+        time.sleep(1)
+        steward_did,steward_verkey = await Steward_did_and_verkey(Wallet_handle)  
+        time.sleep(1)
+        steward_did_for_faber,steward_verkey_for_faber = await steward_keys_for_faber(Wallet_handle)  
+        time.sleep(1)
+        build_nym = await add_to_ledger(Wallet_handle,steward_did,steward_verkey,steward_did_for_faber,steward_verkey_for_faber,pool_handle)
+        time.sleep
+        print('steward_did and verkey for faber = %s %s' % (steward_did_for_faber, steward_verkey_for_faber))
+        steward = input("steward's DID and verkey? ").strip().split(' ')
+        return Wallet_handle,steward_did_for_faber ,steward_verkey_for_faber ,steward[0],steward[1]
+    return Wallet_handle, steward_did,steward_verkey,steward_did_for_faber,steward_verkey_for_faber
+
+
+    
+async def read(Wallet_handle, steward_verkey):
+    with open('message.dat', 'rb') as f:
+        encrypted = f.read()
+    decrypted = await crypto.auth_decrypt(Wallet_handle, steward_verkey, encrypted)
+    # decrypted = await crypto.anon_decrypt(wallet_handle, my_vk, encrypted)
+    print(decrypted)
+
+async def demo():
+    Wallet_handle, steward_did,steward_verkey,steward_did_for_faber,steward_verkey_for_faber = await init()
+    
+
+    while True:
+        argv = input('> ').strip().split(' ')
+        cmd = argv[0].lower()
+        rest = ' '.join(argv[1:])
+        if re.match(cmd, 'prep'):
+            await prep(Wallet_handle, steward_verkey, steward_verkey_for_faber, rest)
+        elif re.match(cmd, 'read'):
+            await read(Wallet_handle, steward_verkey)
+        elif re.match(cmd, 'quit'):
+            break
+        else:
+            print('Huh?')
+
+if __name__ == '__main__':
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(demo())
+        time.sleep(1)  # waiting for libindy thread complete
+    except KeyboardInterrupt:
+        print('')
---
+++ Demo features/Write_DID_Func.py
@@ -0,0 +1,133 @@
+#These steps only include steps up to the point of generating and storing the trust anchor DID and verkey. The latter is stored in the same wallet as that for the steward
+
+
+import asyncio
+import json
+import pprint
+
+from indy import pool, ledger, wallet, did
+from indy.error import IndyError, ErrorCode
+
+from utils import get_pool_genesis_txn_path, PROTOCOL_VERSION
+
+pool_name = 'pool'
+genesis_file_path = get_pool_genesis_txn_path(pool_name)
+
+
+
+
+def print_log(value_color="", value_noncolor=""):
+    """set the colors for text."""
+    HEADER = '\033[92m'
+    ENDC = '\033[0m'
+    print(HEADER + value_color + ENDC + str(value_noncolor))
+
+
+async def pool_configuration():
+    
+    
+        await pool.set_protocol_version(PROTOCOL_VERSION)
+
+     
+        pool_config = json.dumps({'genesis_txn': str(genesis_file_path)})
+        print_log('\n1. Create new pool ledger configuration to connect to ledger.\n')
+
+        try:
+            await pool.create_pool_ledger_config(config_name=pool_name, config=pool_config)
+        except IndyError:
+            await pool.delete_pool_ledger_config(config_name=pool_name)
+            await pool.create_pool_ledger_config(config_name=pool_name, config=pool_config)
+        print_log('\n2. Open ledger and get handle\n')
+        pool_handle = await pool.open_pool_ledger(config_name=pool_name, config=None)
+        return pool_handle
+
+    
+
+async def create_steward_wallet():
+    print_log('\n. Create steward wallet.\n')   
+    wallet_config = json.dumps({"id": "wallet"})
+    wallet_credentials = json.dumps({"key": "wallet_key"})
+    print_log('\n. Open Steward Wallet and Get Handle.\n')
+    try:
+        await wallet.create_wallet(wallet_config, wallet_credentials)
+    except:
+        pass
+    Wallet_handle = await wallet.open_wallet(wallet_config, wallet_credentials)
+    print('wallet = %s' % Wallet_handle)
+    return (Wallet_handle)
+
+async def Steward_did_and_verkey(Wallet_handle):
+    print_log('\n. Generate and store steward DID and verkey\n')
+    
+    steward_did, steward_verkey = await did.create_and_store_my_did(Wallet_handle, "{}")
+   
+    print('steward_did and verkey = %s %s' % (steward_did, steward_verkey))   
+    return steward_did, steward_verkey
+
+
+async def create_faber_wallet():
+    wallet_config = json.dumps({"id": "wallet_faber"})
+    wallet_credentials = json.dumps({"key": "wallet_key_faber"})
+    # 1. Create Wallet and Get Wallet Handle
+    
+    print_log('\n. Create faber wallet.\n')          
+
+    try:  
+        await wallet.create_wallet(wallet_config, wallet_credentials)
+    except IndyError:
+        await wallet.delete_wallet(wallet_config, wallet_credentials)
+        await wallet.create_wallet(wallet_config, wallet_credentials)
+
+    print_log('\n. Open Faber Wallet and Get Handle.\n')
+    faber_handle = await wallet.open_wallet(wallet_config, wallet_credentials)
+
+    return faber_handle
+
+
+
+async def Faber_did_and_verkey(faber_handle):
+
+        print_log('\n. Generate and store Faber did and verkey.\n')
+        try:
+           Faber_did, Faber_verkey = await did.create_and_store_my_did(faber_handle, "{}")
+        except IndyError as ex:
+            if ex.error_code == ErrorCode.DidAlreadyExistsError:
+                pass
+
+        print ('faber_did and verkey = %s %s' % (Faber_did, Faber_verkey))
+        return Faber_did , Faber_verkey
+
+async def steward_keys_for_faber(Wallet_handle):
+   
+    print_log('\n Generate and store steward DID and verkey for faber\n')
+    
+    steward_did_for_faber, steward_verkey_for_faber = await did.create_and_store_my_did(Wallet_handle, "{}")
+   
+    print ('steward_did_for_faber and verkey = %s %s' % (steward_did_for_faber, steward_verkey_for_faber))   
+    return steward_did_for_faber, steward_verkey_for_faber
+
+async def add_to_ledger(Wallet_handle,steward_did,steward_verkey,steward_did_for_faber,steward_verkey_for_faber,pool_handle):
+
+        print_log('\n7. Building NYM request to add steward did and verkey for faber to the ledger\n')
+        nym_transaction_request = await ledger.build_nym_request(submitter_did=steward_did,
+                                                                 target_did= steward_did_for_faber,
+                                                                 ver_key=steward_verkey_for_faber,
+                                                                 alias=None,
+                                                                 role='TRUST_ANCHOR')
+        print_log('NYM transaction request: ')
+        pprint.pprint(json.loads(nym_transaction_request))
+
+        # 8.
+        print_log('\n8. Sending NYM request to the ledger\n')
+        nym_transaction_response = await ledger.sign_and_submit_request(pool_handle=pool_handle,
+                                                                        wallet_handle=Wallet_handle,
+                                                                        submitter_did=steward_did,
+                                                                        request_json=nym_transaction_request)
+        print_log('NYM transaction response: ')
+        pprint.pprint(json.loads(nym_transaction_response))
+
+        return(nym_transaction_request,nym_transaction_response)
+
+async def connection_reques():
+
+   print_log('Send connection request:WORK IN PROGRESS ')
